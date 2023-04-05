fe07f3f1273e54e498584d7548b70ed84b20ca36
Andrey Belchenko <abelchenko@conteq.ru>
2023-04-05 03:21:03 +0300

    1

---
+++ src1/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SegmentDiagramTest from './SegmentDiagramTest';
+
+class App extends React.Component {
+  
+
+  render() {
+    return (
+      <SegmentDiagramTest/>
+    );
+  }
+}
+
+export default App;
